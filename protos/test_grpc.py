93d667124e5d94ab9660c3cccba3b17fdce5dfe0
colosss <kocnevs.jk@yandex.ru>
2026-02-10 16:35:29 +0300

    Add file function

--- protos/test_grpc.py
+++ protos/test_grpc.py
@@ -1,12 +1,26 @@
 from pb import ping_pb2
+from pathlib import Path
 
-def test()->None:
+BASE_PATH=Path(__file__).resolve().parent
+PING_FILEPATH=BASE_PATH/'PING_FILE'
+
+def write()->None:
     ping = ping_pb2.Ping()
     ping.ok=True
     print("ping:", ping.SerializeToString())
+    with PING_FILEPATH.open("wb") as f:
+        f.write(ping.SerializeToString())
 
-def main()->None:
-    test()
+def read()->None:
+    ping=ping_pb2.Ping()
+    with PING_FILEPATH.open("rb") as f:
+        ping.ParseFromString(f.read())
+    print("ping ok: ", ping.ok)
+    print("", ping)
 
+def main()->None:  
+    write()
+    read()
+    
 if __name__=='__main__':
     main()
