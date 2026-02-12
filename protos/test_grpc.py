3ed8542a55d35ef017d47110ed516b617b23e63a
colosss <kocnevs.jk@yandex.ru>
2026-02-12 15:56:36 +0300

    Add grpc-tools

--- protos/test_grpc.py
+++ protos/test_grpc.py
@@ -6,21 +6,25 @@ PING_FILEPATH=BASE_PATH/'PING_FILE'
 
 def write()->None:
     ping = ping_pb2.Ping()
+    print("[first before] ping is initialized", ping.IsInitialized())
     ping.ok=True
+    print("[first after] ping is initialized", ping.IsInitialized())
     print("ping:", ping.SerializeToString())
     with PING_FILEPATH.open("wb") as f:
         f.write(ping.SerializeToString())
 
 def read()->None:
     ping=ping_pb2.Ping()
+    print("[reading before] ping is initialized", ping.IsInitialized())
     with PING_FILEPATH.open("rb") as f:
         ping.ParseFromString(f.read())
+    print("[reading after] ping is initialized", ping.IsInitialized())
     print("ping ok: ", ping.ok)
     print("", ping)
 
 def main()->None:  
     write()
     read()
-    
+
 if __name__=='__main__':
     main()
---
+++ protos/test_grpc_client.py
@@ -0,0 +1,8 @@
+import asyncio
+import logging
+
+import grpc
+import pb.ping_pb2
+import pb.ping_pb2_grpc
+
+
